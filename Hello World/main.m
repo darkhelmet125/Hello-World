46a48caba04234da8f37c1d83d0bc05230f93eb2
Matthew Short <darkhelmet125@yahoo.com>
2013-01-31 16:12:16 -0800

    Initial Commit

---
+++ Hello World/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by William Short on 1/31/13.
+//  Copyright (c) 2013 William Short. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
